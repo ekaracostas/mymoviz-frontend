d4a4b7e405dd6698c550a8ddb8d7b9380e4b0c8b
Emile Karacostas <emile.karacostas@proton.me>
2025-02-05 15:20:46 +0100

    updated fetch

--- components/Home.js
+++ components/Home.js
@@ -36,7 +36,7 @@ function Home() {
 
   // Movies list
   useEffect(() => {
-    fetch(`https://verceldemo-backend.vercel.app/movies`)
+    fetch(`https://mymoviz-backend-sigma-ten.vercel.app/movies`)
     .then(response => response.json())
     .then(data => {
       const newTab = [];
